8512dc5956c95b423736305faa495b38ed52c2c4
Debbie Leiva <debbieleiva@Debbies-MBP.attlocal.net>
2019-10-13 23:50:41 -0400

    milestone2

---
+++ assets/js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	"use strict";
+
+
+
+    // 3. welcome animation support
+
+        $(window).load(function(){
+        	$(".welcome-hero-txt h2,.welcome-hero-txt p").removeClass("animated fadeInUp").css({'opacity':'0'});
+            $(".welcome-hero-serch-box").removeClass("animated fadeInDown").css({'opacity':'0'});
+        });
+
+        $(window).load(function(){
+        	$(".welcome-hero-txt h2,.welcome-hero-txt p").addClass("animated fadeInUp").css({'opacity':'0'});
+            $(".welcome-hero-serch-box").addClass("animated fadeInDown").css({'opacity':'0'});
+        });
+
+	
+
+});
+
+
+
